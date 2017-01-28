158cb57c95fc1fea5131f600f5fc62fd9c677c2f
Sekshey <sekshey@gmail.com>
2017-01-28 22:24:05 +0530

    Revert "left in between creating 2 dynamic componenets"
    This reverts commit 25bcdc795260cffefc3591d3de5ce1923c89eccc.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,21 +2,11 @@ import React, {Component,Proptypes} from 'react';
 import {render} from 'react-dom';
 
 import {Greeter} from './Greeter.js';
-import {Field} from './Field.js';
 
 export class App extends Component {
-  handleNewName(name){
-  this.setState({
-    name: name
-  });
-  };
-  render() {
-      var name=this.state.name;
+    render() {
         return (
-          <div>
-            <Greeter name={name}/>
-            <Field onNewName={this.handleNewName.bind(this)}/>
-          </div>
+          <Greeter name='Sekshey'/>
         );
     };
 };
