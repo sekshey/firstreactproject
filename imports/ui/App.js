4443d1804a016866cbdf598bb1f8738dcb48e9d1
Sekshey <sekshey@gmail.com>
2017-01-28 08:18:50 +0530

    Hello World!

---
+++ imports/ui/App.js
@@ -0,0 +1,10 @@
+import React, {Component, Proptypes} from 'react';
+import {render} from 'react-dom';
+
+export class App extends Component{
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  };
+};
